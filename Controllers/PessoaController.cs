aebaf6d05b5c00d5769af64cf360ea16b85e48d8
ivanzeitoun <ivanzeitoun@gmail.com>
2019-08-06 21:47:03 -0300

    Projeto com formularios

---
+++ Controllers/PessoaController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreMvcControllersActions.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreMvcControllersActions.Controllers
+{
+    public class PessoaController : Controller
+
+    {
+        public IActionResult Formulario()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            Pessoa pessoa = new Pessoa();
+            pessoa.Nome = "Ivan";
+            pessoa.Telefone = "999-999";
+            return View("Pessoa", pessoa);
+        }
+
+        [HttpPost]
+        public ViewResult Formulario (Pessoa pessoa)
+        {
+            return View("DetalhesPessoa", pessoa);
+        }
+
+    }
+}
